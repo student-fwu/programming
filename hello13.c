112d77b0e7e163a821856d4203120fdb07199a44
student-fwu <155794392+student-fwu@users.noreply.github.com>
2024-01-18 18:37:05 +0545

    Add files via upload

---
+++ hello13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    int n;
+    printf("enter number:");
+    scanf("%d", &n);
+
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    printf("final factorial is %d", fact);
+    return 0;
+}
